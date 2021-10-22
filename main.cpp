ac886566c22c8b7e3ae42123f8d6420de481c8ab
Brandon Jarrell <brandonjarrell106@gmail.com>
2021-10-22 15:01:14 -0500

    Completed the sanitization structure, TODO: Complete the mitigate functions

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 #include <cstring>
-#include <map>
 using namespace std;
 
-
-
-std::map<char, char> valid_ = {
-    { 'A', '1' },
-    { 'B', '2' },
-    { 'C', '3' }
-};
 
 /************************
 * SQL Query Generator
@@ -19,184 +11,269 @@ string genQuery(string username, string password)
 {
 	/*cout << "SELECT authenticate\n\t"
 		<< "WHERE " << username << "\n\t\tAT " << password << endl;*/
-	return ("SELECT authenticate WHERE " + username + " and " + password);
+	return ("SELECT * FROM authenticate WHERE UNAME = '" + username + "' AND PASSW = '" + password+ "'");
 }
 
 
 /************************
 * WEAK Mitigation
 *************************/
-string mitigateWeak(string username, string password)
+string mitigateWeak(string inputToBeSan)
 {
 	/*cout << "SELECT authenticate\n\t"
 		<< "WHERE " << username << "\n\t\tAT " << password << endl;*/
-	return ("SELECT authenticate WHERE " + username + " AT " + password);
+	return (inputToBeSan);
+	
 }
 
 
 /************************
 * STRONG Mitigation
 *************************/
-string mitigateStrong(string username, string password)
+string mitigateStrong(string inputToBeSan)
 {
 	/*cout << "SELECT authenticate\n\t"
 		<< "WHERE " << username << "\n\t\tAT " << password << endl;*/
-	return ("SELECT authenticate WHERE " + username + " AT " + password);
+	return (inputToBeSan);
 }
 
 /************************
 * Valid Usernames & Passwords
 *************************/
-string testValid()
+void testValid()
 {
-	// Brnadon
-	// "BrAnD0n_JaRrel", "I_love_My_m0m"; // SELECT WHERE "BrAnD0n_JaRrel" AT "I_love_My_m0m"
+	cout << "Testing Valids:\n\n";
+	for (int i = 0; i < 6; i++) // because there are 5 of us
+	{
+		switch (i)
+		{
+		case 1:  // Brandon
+			cout << endl << "Brandon:\n" << "Normal: " << genQuery("BrAnD0n_JaRrel", "I_love_My_m0m") << endl; // SELECT WHERE "BrAnD0n_JaRrel" AT "I_love_My_m0m"
+			cout << "Weak: " << genQuery(mitigateWeak("BrAnD0n_JaRrel"), mitigateWeak("I_love_My_m0m")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("BrAnD0n_JaRrel"), mitigateStrong("I_love_My_m0m")) << endl;
+			break;
 
-	// Ian
-	genQuery("Outer_Space_TROLL", "SporeCaptain$piceTr@der"); // SELECT WHERE "Outer_Space_TROLL" AND "SporeCaptain$piceTr@der"
+		case 2: // Jared
+			cout << endl << "Jared:\n" << "Normal: " << genQuery("jarebear", "fuzyWuzy4ever!") << endl; //SELECT WHERE jarebear fuzyWuzy4ever!
+			cout << "Weak: " << genQuery(mitigateWeak("jarebear"), mitigateWeak("fuzyWuzy4ever!")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("jarebear"), mitigateStrong("fuzyWuzy4ever!")) << endl;
+			break;
 
-	// Nicole
-	genQuery("PennyAna", "sootSprite"); //SELECT WHERE PennyAna sootSprite
+		case 3: // Ian
+			cout << endl << "Ian:\n" << "Normal: " << genQuery("Outer_Space_TROLL", "SporeCaptain$piceTr@der") << endl; // SELECT WHERE "Outer_Space_TROLL" AND "SporeCaptain$piceTr@der"
+			cout << "Weak: " << genQuery(mitigateWeak("Outer_Space_TROLL"), mitigateWeak("SporeCaptain$piceTr@der")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("Outer_Space_TROLL"), mitigateStrong("SporeCaptain$piceTr@der")) << endl;
+			break;
 
-	// Kevin
-	genQuery("kevin_j0_ramos", "SuPeR_64"); //SELECT WHERE kevin_j0_ramos SuPeR_64 
-	
+		case 4: //Nicole
+			cout << endl << "Nicole:\n" << "Normal: " << genQuery("PennyAna", "sootSprite") << endl; //SELECT WHERE PennyAna sootSprite
+			cout << "Weak: " << genQuery(mitigateWeak("PennyAna"), mitigateWeak("sootSprite")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("PennyAna"), mitigateStrong("sootSprite")) << endl;
+			break;
 
-	// Jared
-	genQuery("jarebear", "fuzyWuzy4ever!"); //SELECT WHERE jarebear fuzyWuzy4ever!
-	//return genQuery(username, password)
-	return " ";
+		case 5: // Kevin
+			cout << endl << "Kevin:\n" << "Normal: " << genQuery("kevin_j0_ramos", "SuPeR_64") << endl; //SELECT WHERE kevin_j0_ramos SuPeR_64 
+			cout << "Weak: " << genQuery(mitigateWeak("kevin_j0_ramos"), mitigateWeak("SuPeR_64")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("kevin_j0_ramos"), mitigateStrong("SuPeR_64")) << endl;
+			break;
+		}
+	}
+	cout << endl << "\t--End of Valid Tests--" << endl;
 }
+
 /************************
 * Tautology Attacks
 *************************/
-string testTautology()
+void testTautology()
 {
-	// Brnadon
-	// 
+	cout << endl << "Testing Tautology:\n\n";
+	for (int i = 0; i < 6; i++) // because there are 5 of us
+	{
+		switch (i)
+		{
+		case 1:  // Brandon
+			cout << endl << "Brandon:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "BrAnD0n_JaRrel" AT "I_love_My_m0m"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Ian
+		case 2: // Jared
+			cout << endl << "Jared:\n"  "Normal: " << genQuery("", "") << endl; //SELECT WHERE jarebear fuzyWuzy4ever!
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Nicole 
-	string tauntaun = 'Root';
-	string rebelPassword = 'vader\' OR \' 'x' \' = \' 'x'';
-	genQueryStrong(tauntaun, rebelPassword);
-	genQueryWeak(tauntaun, rebelPassword);
+		case 3: // Ian
+			cout << endl << "Ian:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "Outer_Space_TROLL" AND "SporeCaptain$piceTr@der"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Kevin
-	genQuery("")
-	
-	// Jared
-	// cout << genQuery(username, password);
-	//cout << genQuery(mitigateStrong(username, password));
-	// cout << genQuery(mitigationWeak(username,password));
-	return " ";
+		case 4: //Nicole
+			cout << endl << "Nicole:\n" << "Normal: " << genQuery("Root", "vader\' OR \' 'x' \' = \' 'x'") << endl; //
+			cout << "Weak: " << genQuery(mitigateWeak("Root"), mitigateWeak("vader\' OR \' 'x' \' = \' 'X'")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("Root"), mitigateStrong("vader\' OR \' 'x' \' = \' 'x'")) << endl;
+			break;
+
+		case 5: // Kevin
+			cout << endl << "Kevin:\n" << "Normal: " << genQuery("", "") << endl; //
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
+		}
+		cout << endl;
+	}
+	cout << endl << "\t--End of Tautology Tests--" << endl;
 }
+
 /************************
 * Union Attacks
 *************************/
-string testUnion()
+void testUnion()
 {
-	// Brnadon
+	cout << endl << "Testing Union:\n\n";
+	for (int i = 0; i < 6; i++) // because there are 5 of us
+	{
+		switch (i)
+		{
+		case 1:  // Brandon
+			cout <<endl << "Brandon:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "BrAnD0n_JaRrel" AT "I_love_My_m0m"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Ian
+		case 2: // Jared
+			cout << endl << "Jared:\n"  "Normal: " << genQuery("", "") << endl; //SELECT WHERE jarebear fuzyWuzy4ever!
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Nicole
-	string tauntaun = '';
-	string rebelPassword = '';
-	genQueryStrong(tauntaun, rebelPassword);
-	genQueryWeak(tauntaun, rebelPassword);
+		case 3: // Ian
+			cout << endl << "Ian:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "Outer_Space_TROLL" AND "SporeCaptain$piceTr@der"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Kevin
-	genQuery("hack4hugs", "balgone UNION SELECT overideRoot FROM adminPassWordList");
+		case 4: //Nicole
+			cout << endl << "Nicole:\n" << "Normal: "<< genQuery("I\'mAUser", "x' UNION SELECT credit_card FROM Customers") << endl; //SELECT WHERE PennyAna sootSprite
+			cout << "Weak: " << genQuery(mitigateWeak("I\'mAUser"), mitigateWeak("x' UNION SELECT credit_card FROM Customers")) << endl;               
+			cout << "Strong: " << genQuery(mitigateStrong("I\'mAUser"), mitigateStrong("x' UNION SELECT credit_card FROM Customers")) << endl;
+			break;
 
-	// Jared
-
-	return " ";
+		case 5: // Kevin
+			cout << endl << "Kevin:\n" << "Normal: " << genQuery("hack4hugs", "balgone UNION SELECT overideRoot FROM adminPassWordList") << endl; //SELECT WHERE kevin_j0_ramos SuPeR_64 
+			cout << "Weak: " << genQuery(mitigateWeak("hack4hugs"), mitigateWeak("balgone UNION SELECT overideRoot FROM adminPassWordList")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("hack4hugs"), mitigateStrong("balgone UNION SELECT overideRoot FROM adminPassWordList")) << endl;
+			break;
+		}
+		cout << endl;
+	}
+	cout << endl << "\t--End of Union Tests--" << endl;
 }
+
 /************************
 * Additional Statement Attacks
 *************************/
-string testAddState()
+void testAddState()
 {
-	// Brnadon
+	
+	cout<<endl << "Testing AddState:\n\n";
+	for (int i = 0; i < 6; i++) // because there are 5 of us
+	{
+		switch (i)
+		{
+		case 1:  // Brandon
+			cout << endl << "Brandon:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "BrAnD0n_JaRrel" AT "I_love_My_m0m"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Ian
+		case 2: // Jared
+			cout << endl << "Jared:\n"  "Normal: " << genQuery("", "") << endl; //SELECT WHERE jarebear fuzyWuzy4ever!
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Nicole
-	string tauntaun = '';
-	string rebelPassword = '';
-	genQueryStrong(tauntaun, rebelPassword);
-	genQueryWeak(tauntaun, rebelPassword);
+		case 3: // Ian
+			cout << endl << "Ian:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "Outer_Space_TROLL" AND "SporeCaptain$piceTr@der"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Kevin
-	genQueryStrong("passwordsgoesbyebye", "; DROP COLUMN passwordList")
+		case 4: //Nicole
+			cout <<endl << "Nicole:\n" << "Normal: " << genQuery("Bob", "42'; INSERT INTO Customer(name, password) VALUES 'Isaac Asimov', 'Foundation'") << endl; //SELECT WHERE PennyAna sootSprite
+			cout << "Weak: " << genQuery(mitigateWeak("Bob"), mitigateWeak("42'; INSERT INTO Customer(name, password) VALUES 'Isaac Asimov', 'Foundation'")) << endl;        ;
+			cout << "Strong: " << genQuery(mitigateStrong("Bob"), mitigateStrong("42'; INSERT INTO Customer(name, password) VALUES 'Isaac Asimov', 'Foundation'")) << endl;
+			break;
 
-	// Jared
-
-	return " ";
+		case 5: // Kevin
+			cout << endl << "Kevin:\n" << "Normal: " << genQuery("passwordsgoesbyebye", "; DROP COLUMN passwordList") << endl; //SELECT WHERE kevin_j0_ramos SuPeR_64 
+			cout << "Weak: " << genQuery(mitigateWeak("passwordsgoesbyebye"), mitigateWeak("; DROP COLUMN passwordList")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("passwordsgoesbyebye"), mitigateStrong("; DROP COLUMN passwordList")) << endl;
+			break;
+		}
+		cout << endl;
+	}
+	cout << endl << "\t--End of AddState Tests--" << endl;
 }
+
 /************************
 * Comment Attacks
 *************************/
-string testComment()
+void testComment()
 {
-	// Brnadon
+	cout<< endl << "Testing Comments:\n\n";
+	for (int i = 0; i < 6; i++) // because there are 5 of us
+	{
+		switch (i)
+		{
+		case 1:  // Brandon
+			cout << endl << "Brandon:\n" << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "BrAnD0n_JaRrel" AT "I_love_My_m0m"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Ian
+		case 2: // Jared
+			cout << endl << "Jared:\n"  "Normal: " << genQuery("", "") << endl; //SELECT WHERE jarebear fuzyWuzy4ever!
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
-	// Nicole
-	string tauntaun = '';
-	string rebelPassword = '';
-	genQueryStrong(tauntaun, rebelPassword);
-	genQueryWeak(tauntaun, rebelPassword);
+		case 3: // Ian
+			cout << endl << "Normal: " << genQuery("", "") << endl; // SELECT WHERE "Outer_Space_TROLL" AND "SporeCaptain$piceTr@der"
+			cout << "Weak: " << genQuery(mitigateWeak(""), mitigateWeak("")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong(""), mitigateStrong("")) << endl;
+			break;
 
+		case 4: //Nicole
+			cout << endl <<"Normal: " << genQuery("Root'; --", "chilledmonkeybrains") << endl; //SELECT WHERE PennyAna sootSprite
+			cout << "Weak: " << genQuery(mitigateWeak("Root'; --"), mitigateWeak("chilledmonkeybrains")) << endl;           
+			cout << "Strong: " << genQuery(mitigateStrong("Root'; --"), mitigateStrong("chilledmonkeybrains")) << endl;
+			break;
 
-	// Kevin
-	genQuery("BrotherPhillips'", "; --"); 
-
-	// Jared
-
-	return " ";
+		case 5: // Kevin
+			cout << endl << "Kevin:\n" << "Normal: " << genQuery("BrotherPhillips'", "; --") << endl; //SELECT WHERE kevin_j0_ramos SuPeR_64 
+			cout << "Weak: " << genQuery(mitigateWeak("BrotherPhillips'"), mitigateWeak("; --")) << endl;
+			cout << "Strong: " << genQuery(mitigateStrong("BrotherPhillips'"), mitigateStrong("; --")) << endl;
+			break;
+		}
+		cout << endl;
+	}
+	cout << endl << "\t--End of Comment Tests--" << endl;
 }
-/************************
-*  END OF TESTS
-*************************/
 
 
 
 int main()
 {
-	/*
-	Main calls all tests which run genQuery on each string set
-	Main calls all tests which run genQueryWeak on each string set then through genQuery
-	Main calls all tests which run genQueryStrong on each string set then through genQuery
-	*/
-	string username;
-	string password;
 
-	cout << "\n\tSQL Injection Sanitization\n\n"
-		<< "\tPlease enter a User name and Password\n";
-
-	cout << "\tUsername: ";
-	cin >> username;
-
-	cout << "\tPassword: ";
-	cin >> password;
-
-	cout << "\n\tUsername is \"" << username << "\" and the " << "Password is \"" << password << "\"\n";
-
-	cout << "\nNormal genQuery: \t" << genQuery(username, password);
-	cout << "\nWeak genQuery: \t" << genQueryWeak(username, password);
-	cout << "\nStronggenQuery: \t" << genQueryStrong(username, password);
+	cout << "\n\tSQL Injection Sanitization\n\n";
 	
-	/*
 	testValid();
 	testTautology();
 	testUnion();
 	testAddState();
 	testComment();
-	*/
 
 
 	return 0;
